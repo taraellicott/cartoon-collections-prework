726e9f97dfb4bd9a01c1611d4e0f817e1619fd8e
Tara Ellicott <taraellicott@gmail.com>
2018-04-17 00:30:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,17 @@
 def roll_call_dwarves(dwarf)
-  dwarf.each do |ind|
-    
+  dwarf.each.index do
   puts "#{ind} #{dwarf}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+# def summon_captain_planet# code an argument here
+#   # Your code here
+# end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
