a782606e615a5b6656c1427f593c5ec6f43967aa
Tara Ellicott <taraellicott@gmail.com>
2018-04-17 00:48:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteers)
- planeteers.map.capitalize! do ||
- 
+ planeteers.map. do ||
+ capitalize!
 end
 
 # def long_planeteer_calls# code an argument here
