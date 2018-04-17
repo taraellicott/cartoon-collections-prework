e5f1c3d7f641015eece3470ef20994ee41a49c04
Tara Ellicott <taraellicott@gmail.com>
2018-04-17 00:35:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves(dwarf)
-  dwarf.each.with_index(1) do |dwarf,index|
+def roll_call_dwarves(dwarves)
+  dwarves.each.index(1) do |dwarf,index|
   puts "#{index}. #{dwarf}"
 end
 end
