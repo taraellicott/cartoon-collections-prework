49951561cb3ec21bac44a7b13d16d7208dd5988b
Tara Ellicott <taraellicott@gmail.com>
2018-04-17 01:25:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(calls_array)
   calls_array.any? {|calls| calls.length > 4}
 end
 
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
+# def find_the_cheese(array)
+array.include?
+
 #   cheese_types = ["cheddar", "gouda", "camembert"]
 # end
